6dac87063d3f5a732a94157f69b3e482517eb190
NishPatel101 <nishpatel101@yahoo.com>
2021-06-06 18:50:42 -0700

    Added timer for user: DEMOABLE

--- source/main.c
+++ source/main.c
@@ -21,6 +21,36 @@
 #include "simAVRHeader.h"
 #endif
 
+void set_PWM(double frequency) {
+    static double current_frequency;
+    if (frequency != current_frequency) {
+        if(!frequency) 
+            TCCR3B &= 0x08; // stops timer/counter
+        else
+            TCCR3B |= 0x03; // resumes/continues timer/counter
+        if(frequency < 0.954)
+            OCR3A = 0xFFFF;
+        else if (frequency > 31250)
+            OCR3A = 0x0000;
+        else
+            OCR3A = (short) (8000000 / (128 * frequency)) - 1;
+        TCNT3 = 0; // resets counter
+        current_frequency = frequency; // Updates the current frequency
+    }
+}
+
+void PWM_on() {
+    TCCR3A = (1 << COM3A0); // COM3A0: Toggle PB3 on compare match between counter and OCR3A
+    TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+    set_PWM(0);
+}
+
+void PWM_off() {
+    TCCR3A = 0x00;
+    TCCR3B = 0x00;
+}
+
+
 // Signalers/Gatekeepers
 unsigned begin = 0;
 unsigned char needNewProb = 1;
@@ -29,7 +59,7 @@ unsigned int time1 = 0, time2 = 0;
 // Game values
 unsigned char userSol[] = {'\0', '\0', '\0'};
 int score = 0;
-unsigned char lives = 3;
+unsigned char lives = 5;
 unsigned char x; // Keypad Key
 
 enum Rand_States { R_Wait, R_Pressed, R_Finished };
@@ -115,7 +145,7 @@ int Problem(int state) {
 
 		case 3: // Division
             	    a = rand() % 17;
-            	    b = rand() % 17;
+            	    b = (rand() % 16) + 1; // Avoid 0 in denominator
 
 	    	    answer = times_table[a][b];
 	    	    int temp = a; // Swapping dividend and quotient
@@ -133,33 +163,61 @@ int Problem(int state) {
     return state;
 }
 
-/*
-enum Speaker_States { S_Start };
+double title[] = {261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25};
+unsigned char note = 0;
+enum Speaker_States { S_Title, S_Done };
 int Speaker(int state) {
     switch (state) { // Transitions
-	case S_Start:
-	    state = ;
+	case S_Title:
+	    if (note == 8)
+		state = S_Done;
+	    else {
+		set_PWM(title[note]);
+	        note++;
+	    }
+	    break;
+
+	case S_Done:
+	    set_PWM(0.0);
 	    break;
 
 	default:
-	    state = S_Start;
+	    state = S_Title;
     }
-
-    switch (state) { // Actions
-	case S_Start:
+    return state;
+}
+/*
+enum Timer_States { T_Start, T_Wait};
+int Timer(int state) {
+    switch(state) {
+	case T_Start:
+	    if (begin)
+		state = T_Wait;
 	    break;
+
+	case T_Wait:
+	    if (!needNewProb)
+		state = T_Light;
+	    break;
+
+	case T_Light:
+	    b;
+	    break;
+
+	default:
+	    state = T_Start;
     }
     return state;
 }
 */
-
-enum Game_States { G_Title, G_Start, G_WaitBegin, G_NewProblem, G_Problem_Wait, G_Problem_Pressed, G_Correct, G_Incorrect, G_PostProbDisplay, G_Lost };
+enum Game_States { G_Title, G_Start, G_WaitBegin, G_NewProblem, G_Problem_Wait, G_Problem_Pressed, G_Correct, G_SpeedUp, G_Incorrect, G_TimedOut, G_PostProbDisplay, G_Lost };
 char a_str[12]; b_str[8], score_str[8], lives_str[8];
 unsigned short sz_a;
 unsigned char userCursor = 0; // Index in user_answer array
 unsigned char cursor = 0; // Actual LCD cursor for user display
 int user_answer = 0;
 int count = 0;
+unsigned short timePassed = 0, timeLimit = 7000;
 int Game(int state) {
     switch (state) {
 	case G_Title:
@@ -170,7 +228,7 @@ int Game(int state) {
 	
 	case G_Start:
 	    score = 0;
-	    lives = 3;
+	    lives = 5;
 	    if (x == '#')
 		state = G_WaitBegin;
 	    break;
@@ -210,67 +268,68 @@ int Game(int state) {
 	    break;
 
 	case G_Problem_Wait:
-	    if (x) { // User input something
-		if (x == '#') { // User submitted answer
-		    userEntered = 1;
-		}
-		else if (x == '*') { // Backspace ('*')
-		    if (userCursor != 0)
-			userSol[--userCursor] = ' ';
-		        cursor--;
-			LCD_Cursor(sz_a + cursor);
-		}
-		else { // Regular number
-		    if (userCursor < 3) {
-			userSol[userCursor++] = x;
-			cursor++;
-			LCD_Cursor(sz_a + cursor);
+	    if (timePassed >= timeLimit)
+		state = G_TimedOut;
+	    else {
+	    	if (x) { // User input something
+		    if (x == '#') { // User submitted answer
+		        userEntered = 1;
 		    }
-		}
-		LCD_Message(sz_a, userSol);
-		LCD_Message(15, "  ");
-		state = G_Problem_Pressed;
+		    else if (x == '*') { // Backspace ('*')
+		        if (userCursor != 0)
+			    userSol[--userCursor] = ' ';
+		    }
+		    else { // Regular number
+		        if (userCursor < 3)
+			    userSol[userCursor++] = x;
+		    }
+		    LCD_Message(sz_a, userSol);
+		    LCD_Message(15, "  ");
+		    state = G_Problem_Pressed;
+	        }
 	    }
+	    timePassed += 50;
+	    count = 0; // Resetting for result screen
 	    break;
 
 	case G_Problem_Pressed:
-	    if (!x) {
-		// Compare user-input and answer
-                // Equal        ->      G_Correct
-                // Not equal    ->      G_Incorrect ... lives--
-		if (userEntered) { // Pressed # ~ check answer
-		    user_answer = 0;
-		    unsigned char nuls = 0;
-		    unsigned char d = 100;
-		    for (char m = 2; m > -1; m--) {
-			if (userSol[m] == ' ' || !userSol[m])
-			    nuls++;
-		    }
-		    if (nuls == 3)
-			user_answer = -1; // WRONG ANSWER
-		    else {
-			d /= (int) (pow(10, nuls) + 0.5);
-		    	for (int n = 0; n < 3; n++) {
-			    if (userSol[n])
-			    	user_answer += (userSol[n] - 48) * d;
-			    d /= 10;
-		    	}
-		    }
-		    if (user_answer == answer) // CHECK ANSWER
-			state = G_Correct;
-		    else
-			state = G_Incorrect;
+	    if (timePassed >= timeLimit)
+		state = G_TimedOut;
+	    else {
+	    	if (!x) {
+		    // Compare user-input and answer
+                    // Equal        ->      G_Correct
+                    // Not equal    ->      G_Incorrect ... lives--
+		    if (userEntered) { // Pressed # ~ check answer
+		        user_answer = 0;
+		        unsigned char nuls = 0;
+		        unsigned char d = 100;
+		        for (char m = 2; m > -1; m--) {
+			    if (userSol[m] == ' ' || !userSol[m])
+			        nuls++;
+		        }
+		        if (nuls == 3)
+			    user_answer = -1; // WRONG ANSWER
+		        else {
+			    d /= (int) (pow(10, nuls) + 0.5);
+		    	    for (int n = 0; n < 3; n++) {
+			        if (userSol[n])
+			    	    user_answer += (userSol[n] - 48) * d;
+			        d /= 10;
+		    	    }
+		        }
+		        if (user_answer == answer) // CHECK ANSWER
+			    state = G_Correct;
+		        else
+			    state = G_Incorrect;
 			
-		    itoa(answer, answer_str, 10); // Solution string for displaying on next screen
-
-		    userEntered = 0; // Resetting user submission checker for next problem
-		    userSol[0] = '\0'; userSol[1] = '\0'; userSol[2] = '\0'; // Reset array for next problem
-		    userCursor = 0; // Reset user cursor for next problem
-		    count = 0; // Resetting for next screen
-		}
-		else
-		    state = G_Problem_Wait;	
+		        itoa(answer, answer_str, 10); // Solution string for displaying on 'Correct' and 'Incorrect'
+		    }
+		    else
+		        state = G_Problem_Wait;
+	        }
 	    }
+	    timePassed += 50;
 	    break;
 
 	case G_Correct:
@@ -285,6 +344,18 @@ int Game(int state) {
 	    score++;
 	    needNewProb = 1;
 	    state = G_PostProbDisplay;
+	    break;
+
+	case G_SpeedUp:
+	    if (count == 0 || count == 1000 || count == 2000) { // Blink on
+	        LCD_Message(5, "SPEEDING");
+		LCD_Message(24, "UP");
+	    }
+	    else if (count == 500 || count == 1500 || count == 2500) // Blink off
+		LCD_ClearScreen();
+	    else if (count >= 3000) // Screen finished; go to next problem
+		state = G_NewProblem;
+	    count += 50; // Incrementing
 	    break;
 
 	case G_Incorrect:
@@ -303,13 +374,37 @@ int Game(int state) {
 	    state = G_PostProbDisplay;
 	    break;
 
+	case G_TimedOut:
+	    itoa(answer, answer_str, 10); // Solution string for displaying on 'Timed Out'
+	    LCD_ClearScreen();
+	    LCD_Message(4, "Time's up!");
+	    LCD_Message(19, "Solution: ");
+	    LCD_Message(29, answer_str);
+	    lives--;
+	    needNewProb = 1;
+	    state = G_PostProbDisplay;
+	    break;
+
 	case G_PostProbDisplay:
+	    timePassed = 0; // Reset time passed
+	    userEntered = 0; // Resetting user submission checker for next problem
+            userSol[0] = '\0'; userSol[1] = '\0'; userSol[2] = '\0'; // Reset array for next problem
+            userCursor = 0; // Reset user cursor for next problem
 	    count += 50;
-	    if (count == 2000) { // Display 'Correct' or 'Incorrect' screen for 2 seconds
+	    if (count == 1500) { // Display 'Correct','Incorrect' or 'Timed Out' screen for 1.4 seconds
 		if (lives == 0)
 		    state = G_Lost;
+		else if (score == 7 && timeLimit != 6000) { // Second condition if speed up already happened and score stayed the same
+		    state = G_SpeedUp;
+		    timeLimit = 6000;
+		}
+		else if (score == 12 && timeLimit != 5000) {
+		    state = G_SpeedUp;
+		    timeLimit = 5000;
+		}
 		else
 		    state = G_NewProblem;
+		count = 0; // Resetting for next problem's results display
 		LCD_ClearScreen();
 	    }
 	    break;
@@ -352,13 +447,13 @@ int main(void) {
     task1.period = 25; // Task period
     task1.elapsedTime = task1.period; // Task current elapsed time
     task1.TickFct = &Problem; // Function pointer for the tick
-    /*
+    
     // Task 2 (Speaker)
     task2.state = start; // Task initial state
-    task2.period = 200; // Task period
+    task2.period = 600; // Task period
     task2.elapsedTime = task2.period; // Task current elapsed time
     task2.TickFct = &Speaker; // Function pointer for the tick
-    */
+    
     // Task 3 (Game)
     task3.state = start; // Task initial state
     task3.period = 50; // Task period
@@ -377,7 +472,7 @@ int main(void) {
     TimerSet(GCD);
     TimerOn();
 
-    //PWM_on(); set_PWM(0);
+    PWM_on(); set_PWM(0.0);
     LCD_init();
     unsigned short i; // Scheduler for-loop iterator
     while (1) {
